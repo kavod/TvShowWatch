d8a546d049998a24252cf418f206eeecb69cb46b
kavod <brice.grichy@gmail.com>
2013-11-09 15:38:26 +0100

    fix another bug from last commit

--- ConfFile.py
+++ ConfFile.py
@@ -63,7 +63,7 @@ class ConfFile(MyFile):
 		conf = self._create_root()
 		tracker_conf = self.confTracker()
 		tc_conf = self.confTransmission()
-		email_conf = confEmail(self)
+		email_conf = self.confEmail(self)
 
 
 		# Transmission conf
